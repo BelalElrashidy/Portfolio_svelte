11dd8b62219abb86318cbecb5f16de6f825a1ff7
BeboElRashidy <bebo.elrashidy22@gmail.com>
2023-07-12 18:54:22 +0300

    fixing bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Home from '../components/home.svelte';
+</script>
+
+<body>
+	<Home />
+</body>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	body {
+		background-color: #f5f5f5;
+		font-family: sans-serif;
+	}
+</style>
