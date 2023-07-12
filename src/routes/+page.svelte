54cdbed4679dee723df6c6d73874b020fe6daf7f
BeboElRashidy <bebo.elrashidy22@gmail.com>
2023-07-12 18:27:32 +0300

    FIxing bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,2 @@
-<script>
-	import Home from '../components/home.svelte';
-</script>
-
-<body>
-	<Home />
-</body>
-
-<style>
-	* {
-		margin: 0;
-		padding: 0;
-	}
-
-	body {
-		background-color: #f5f5f5;
-		font-family: sans-serif;
-	}
-</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
